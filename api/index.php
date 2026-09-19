8ced43173c9bf5ec7a011ca54a6bebfaf25ae163
GusnaldiRPH <gusnaldi1388@gmail.com>
2026-09-19 20:01:03 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
-// Forward Vercel requests to Laravel's normal entry point
+// Mengalihkan direktori storage Laravel ke folder /tmp Vercel
+$exec = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+];
+
+foreach ($exec as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
